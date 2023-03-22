a9068cb8de970e3970f6d8167a3ed6e79544d9a0
Julie Janssen <julie.janssen@sintlucasedu.nl>
2023-03-22 12:20:32 +0100

    updated againnn

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,16 +1,35 @@
-document.getElementById("mainTitle").innerText="Point and click adventure";
-document.getElementById("mainTitle").style.textAlign= "center"; 
+document.getElementById("mainTitle").innerText = "Point and click adventure";
+document.getElementById("mainTitle").style.textAlign = "center";
 
-const offsetCharacter = 16; 
+const offsetCharacter = 16;
 
 const mainCharacter = document.getElementById("mainCharacter");
 const gameWindow = document.getElementById("gameWindow");
 
-gameWindow.onclick = function(e){
+gameWindow.onclick = function (e) {
     var rect = gameWindow.getBoundingClientRect();
     var x = e.clientX - rect.left;
     var y = e.clientY - rect.top;
     mainCharacter.style.left = x - offsetCharacter + "px";
     mainCharacter.style.top = y - offsetCharacter + "px";
+
     console.log(x);
+
+    console.log(e.target.id);
+    switch (e.target.id) {
+        case "doorOne":
+            console.log("this is door one");
+            // something insert here
+            break;
+
+        case "doorTwo":
+            console.log("this is door two");
+            // something insert here
+            break;
+
+        default:
+            console.log("unavailable clicked item");
+            // do something when it doesn't have a case
+            break;
+    }
 }
