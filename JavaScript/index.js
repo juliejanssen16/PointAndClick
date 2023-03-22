0c40b9bff976c165ca35295be3088d02bd7826ed
Julie Janssen <julie.janssen@sintlucasedu.nl>
2023-03-22 10:54:36 +0100

    update ig

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,1 +1,16 @@
 document.getElementById("mainTitle").innerText="Point and click adventure";
+document.getElementById("mainTitle").style.textAlign= "center"; 
+
+const offsetCharacter = 16; 
+
+const mainCharacter = documet.getElementById("mainCharacter");
+const gameWindow = document.getElementById("gameWindow");
+
+gameWindow.onclick = function(e){
+    var rect = gameWindow.getBoundingClientRect();
+    var x = e.clientX - rect.left;
+    var y = e.clientY - rect.top;
+    mainCharacter.style.left = x - offsetCharacter + "px";
+    mainCharacter.style.top = y - offsetCharacter + "px";
+    console.log(x);
+}
