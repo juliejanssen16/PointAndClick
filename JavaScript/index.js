4b1c05f70247245ca90b2c3b112281877072f63c
Julie Janssen <julie.janssen@sintlucasedu.nl>
2023-03-30 19:46:22 +0200

    major dialog and sound update

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -11,17 +11,20 @@ const characterAudio = document.getElementById("characterAudio");
 const counterCharacter = document.getElementById("counterCharacter");
 let inventory = [];
 const inventoryList = document.getElementById("inventoryList");
+let moneyUser = 1000;
+let socialSkills = 0;
+const walkingCharacter = document.getElementById("walkingCharacter");
 
 gameWindow.onclick = function (e) {
     if (mainCharacterSpeech.style.opacity == 0 && counterSpeech.style.opacity == 0) {
         var rect = gameWindow.getBoundingClientRect();
         var x = e.clientX - rect.left;
         var y = e.clientY - rect.top;
-        playAudio();
+        //playAudio();
         mainCharacter.style.left = x - offsetCharacter + "px";
         mainCharacter.style.top = y - offsetCharacter + "px";
 
-        console.log(x);
+        //console.log(x);
 
         //console.log(e.target.id);
         //showSpeech("Does this work?");
@@ -29,44 +32,98 @@ gameWindow.onclick = function (e) {
         switch (e.target.id) {
 
             case "doorOne":
-                pauseAudio();
-                // characterAudio.play();
+                //pauseAudio();
                 showSpeech(mainCharacterSpeech, characterAudio, "It's locked, I can't get in!");
-                console.log(" butaehfiuhji");
+                socialSkills = socialSkills + 5;
+                console.log(moneyUser);
+                console.log(socialSkills);
+                //console.log(" butaehfiuhji");
                 // something insert here
                 break;
 
             case "doorTwo":
-                pauseAudio();
+                //pauseAudio();
                 showSpeech(mainCharacterSpeech, characterAudio, "It's locked, I can't get in!");
+                socialSkills = socialSkills + 5;
+                console.log(moneyUser);
+                console.log(socialSkills);
                 // something insert here
                 break;
 
             case "infoBord":
                 if (checkItem("information book")) {
                     showSpeech(counterSpeech, targetAudio, "Have a look in your information book, there you can see all of the information you need!");
+                    socialSkills = socialSkills + 5;
+                    console.log(moneyUser);
+                    console.log(socialSkills);
                 }
                 else {
                     showSpeech(counterSpeech, targetAudio, "Go explore! <br> there are no special events today :) <br> Here you can have an information book so you know when there will be something to do!");
                     getItem("information book", "infoBook");
+                    socialSkills = socialSkills + 5;
+                    moneyUser = moneyUser + 45;
+                    console.log(moneyUser);
+                    console.log(socialSkills);
                 }
-                pauseAudio();
+                // pauseAudio();
                 setTimeout(showSpeech, 5 * sec);
                 break;
 
             case "doorThree":
-                pauseAudio();
-                showSpeech(mainCharacterSpeech, characterAudio, "*knock knock* Is anyone home?");
-                setTimeout(function () { counterCharacter.style.opacity = 1; }, 4 * sec);
-                setTimeout(showSpeech, 4 * sec, counterSpeech, targetAudio, "Coming!");
-                setTimeout(showSpeech, 8 * sec, counterSpeech, targetAudio, "Hello darling, how can I help you?");
-                setTimeout(showSpeech, 12 * sec, mainCharacterSpeech, characterAudio, "I would like to buy some strawberries,please.");
-                getItem("strawberries", "strawberries");
-                setTimeout(function () { counterCharacter.style.opacity = 0; }, 16 * sec);
+                //pauseAudio();
+                if (checkItem("strawberries")) {
+                    //pauseAudio();
+                    showSpeech(mainCharacterSpeech, characterAudio, "*knock knock*");
+                    setTimeout(function () { counterCharacter.style.opacity = 1; }, 4 * sec);
+                    setTimeout(showSpeech, 4 * sec, counterSpeech, targetAudio, "Oh it's you again");
+                    setTimeout(showSpeech, 8 * sec, counterSpeech, targetAudio, "How can I help you?");
+                    setTimeout(showSpeech, 12 * sec, mainCharacterSpeech, characterAudio, "Can we talk?");
+                    setTimeout(showSpeech, 16 * sec, counterSpeech, targetAudio, "About what love?");
+                    setTimeout(showSpeech, 20 * sec, mainCharacterSpeech, characterAudio, "I just really need your advice on something. <br> please?");
+                    setTimeout(showSpeech, 24 * sec, counterSpeech, targetAudio, "Alright alright..come in");
+                    setTimeout(showSpeech, 28 * sec, mainCharacterSpeech, characterAudio, "*talking*");
+                    setTimeout(showSpeech, 32 * sec, counterSpeech, targetAudio, "*talking*");
+                    setTimeout(showSpeech, 40 * sec, counterSpeech, targetAudio, "You can always come back when you need me love!");
+                    setTimeout(showSpeech, 45 * sec, mainCharacterSpeech, characterAudio, "Thank you so much, you really helped me!");
+                    socialSkills = socialSkills + 40;
+                    console.log(moneyUser);
+                    console.log(socialSkills);
+                    setTimeout(function () { counterCharacter.style.opacity = 0; }, 50 * sec);
+                }
+                else {
+                    //pauseAudio();
+                    showSpeech(mainCharacterSpeech, characterAudio, "*knock knock* Is anyone there?");
+                    setTimeout(function () { counterCharacter.style.opacity = 1; }, 4 * sec);
+                    setTimeout(showSpeech, 4 * sec, counterSpeech, targetAudio, "Coming!");
+                    setTimeout(showSpeech, 10 * sec, counterSpeech, targetAudio, "Hello darling, how can I help you?");
+                    setTimeout(showSpeech, 14 * sec, mainCharacterSpeech, characterAudio, "I would like to buy some strawberries,please.");
+                    setTimeout(showSpeech, 18 * sec, counterSpeech, targetAudio, "There you go!");
+                    socialSkills = socialSkills + 10;
+                    moneyUser = moneyUser - 3;
+                    console.log(moneyUser);
+                    console.log(socialSkills);
+                    getItem("strawberries", "strawberries");
+                    setTimeout(function () { counterCharacter.style.opacity = 0; }, 22 * sec);
+                }
                 break;
 
+            case "doorFour":
+                //pauseAudio();
+                showSpeech(mainCharacterSpeech, characterAudio, "*knock knock* Hello?");
+                setTimeout(function () { counterCharacter.style.opacity = 1; }, 4 * sec);
+                setTimeout(showSpeech, 4 * sec, counterSpeech, targetAudio, "What do you want?");
+                setTimeout(showSpeech, 8 * sec, mainCharacterSpeech, characterAudio, "I just want to talk to you..");
+                setTimeout(showSpeech, 12 * sec, counterSpeech, targetAudio, "I'm not interested, leave me alone!");
+                setTimeout(showSpeech, 16 * sec, mainCharacterSpeech, characterAudio, "Well jeez, alright then..");
+                getItem("bandage for the broken hearted", "bandage");
+                socialSkills = socialSkills + 10;
+                console.log(moneyUser);
+                console.log(socialSkills);
+                setTimeout(function () { counterCharacter.style.opacity = 0; }, 17 * sec);
+                break
+
             default:
-                playAudio();
+                // playAudio();
                 //hideSpeech(targetBubble, targetAudio);
                 // do something when it doesn't have a case
                 break;
@@ -75,35 +132,35 @@ gameWindow.onclick = function (e) {
 }
 
 
-function showSpeech(targetBubble, targetAudio, dialog) {
+function showSpeech(counterSpeech, targetAudio, dialog) {
     //put a talking sound                  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-    targetBubble.style.opacity = 1;
-    targetBubble.innerHTML = dialog;
-    setTimeout(hideSpeech, 5 * sec, targetBubble, targetAudio);
+    counterSpeech.style.opacity = 1;
+    counterSpeech.innerHTML = dialog;
+    setTimeout(hideSpeech, 5 * sec, counterSpeech, targetAudio);
     targetAudio.currentTime = 0;
     targetAudio.play();
 }
-function hideSpeech(targetBubble, targetAudio) {
+function hideSpeech(counterSpeech, targetAudio) {
     // mainCharacterSpeech.style.opacity = 0;
     //mainCharacterSpeech.innerHTML = "...";
     //characterAudio.pause();
-    targetBubble.style.opacity = 0;
-    targetBubble.innerHTML = "...";
+    counterSpeech.style.opacity = 0;
+    counterSpeech.innerHTML = "...";
     targetAudio.pause();
     //put and sound make it stop after the delay !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 }
 const counterSpeech = document.getElementById("counterSpeech");
-showSpeech(counterSpeech, characterAudio, "Hello sweetheart!");
+showSpeech(counterSpeech, characterAudio, "Hello sweetheart! <br> I hope you'll have fun playing <br> my point and click adventure game!");
 
-var x = document.getElementById("characterAudio");
+//var x = document.getElementById("walkingCharacter");
 
-function playAudio() {
-    x.play();
-}
+//function playAudio() {
+//   x.play();
+//}
 
-function pauseAudio() {
-    x.pause();
-}
+//function pauseAudio() {
+//   x.pause();
+//}
 
 function getItem(itemName, itemId) {
     if (!checkItem(itemName)) {
@@ -136,4 +193,9 @@ function removeItem(itemName, itemId) {
 
     });
     document.getElementById(itemId).remove();
+}
+
+function showMoney(moneyUserTwo) {
+    moneyUserTwo = 1000;
+    document.getElementById("dollarSign").innerHTML(moneyUserTwo);
 }
